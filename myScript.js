62acbfdbb37a0c3181ff2cf1d49d13873bb2907e
theodoz <dozie.udeagha@gmail.com>
2021-11-09 00:05:50 +0000

    index.html

--- myScript.js
+++ myScript.js
@@ -1,86 +1,113 @@
-//getting random play bythe computer
 
-function computerPlay(){
-  function randomIndex(options){
+let playerScore = 1;
+let computerScore = 1;
 
-    const randomPlay = Math.floor(Math.random()* options.length);
+const btn = document.querySelectorAll ('button');
 
-    const item = options[randomPlay];
+btn.forEach((button) => {
+   
+ button.addEventListener('click', function playerSelect(){
 
-    return item;
+    playerSelection = button.textContent;
 
-    
+    //getting random play bythe computer
+   function computerPlay(){
+     function randomIndex(options){
+       const randomPlay = Math.floor(Math.random()* options.length);
+       const item = options[randomPlay];
+       return item;
+       }
 
-  }
+  
 
- const gameOptions = ["ROCK", "SCISSORS", "PAPER"];
+     const gameOptions = ["ROCK", "SCISSORS", "PAPER"];
 
- const result = randomIndex(gameOptions);
+     const result = randomIndex(gameOptions);
 
- alert(result);
+     console.log(result);
+ 
+     const para2 = document.querySelector ('.computerplay');
 
- return result;
+     para2.textContent = "The computer played " + result;
+   
+     return result;
 
+ 
+    }
 
-}
+ 
+     const computerSelection = computerPlay() 
 
 
 // setting the conditions of the game
 
 
-function playRound(playerSelection, computerSelection){
 
-   if (computerSelection == "ROCK" && playerSelection == "PAPER"){
-       alert("You win! Paper beats Rock");
-   } else if (computerSelection === "ROCK" && playerSelection == "SCISSORS"){
-       alert("You loose! Rock beats Scissors");
-   } else if (computerSelection === "ROCK" && playerSelection == "ROCK"){
-       alert("It\'s a draw! Try again");
-   } else if (computerSelection === "PAPER" && playerSelection == "ROCK"){
-       alert("You loose! Paper beats the Rock");
-   } else if (computerSelection === "PAPER" && playerSelection =="SCISSORS"){
-       alert("You win! Scissors beat the Paper");
-   } else if (computerSelection ==="PAPER" && playerSelection == "PAPER"){
-       alert("It\'s a draw! Try again");
-   } else if (computerSelection === "SCISSORS" && playerSelection == "PAPER"){
-       alert("You loose! Scissors beat Paper");
-   } else if (computerSelection === "SCISSORS" && playerSelection == "ROCK"){
-       alert("You win! Rock beats Scissors");
-   } else if (computerSelection === "SCISSORS" && playerSelection == "SCISSORS"){
-       alert("It\'s a draw! Try again");
+
+      const pScore = document.querySelector ('.playerscore')
+      const cScore = document.querySelector('.computerscore')
+
+      const para1 = document.querySelector('.outcome');
+
+
+  function playRound(playerSelection, computerSelection){
+
+     if (computerSelection == "ROCK" && playerSelection == "PAPER"){
+       para1.textContent = "You win! Paper beats Rock"; 
+       pScore.textContent = "Your score is " + playerScore++;
+     } else if (computerSelection === "ROCK" && playerSelection == "SCISSORS"){
+       para1.textContent ="You loose! Rock beats Scissors";
+       cScore.textContent = "Computer score is " + computerScore++;
+     } else if (computerSelection === "ROCK" && playerSelection == "ROCK"){
+       para1.textContent= "It\'s a draw! Try again"
+     } else if (computerSelection === "PAPER" && playerSelection == "ROCK"){
+       para1.textContent = "You loose! Paper beats the Rock";
+       cScore.textContent = "Computer score is " + computerScore++;
+     } else if (computerSelection === "PAPER" && playerSelection =="SCISSORS"){
+       para1.textContent = "You win! Scissors beat the Paper";
+       pScore.textContent = "Your score is " + playerScore++;
+     } else if (computerSelection ==="PAPER" && playerSelection == "PAPER"){
+       para1.textContent = "It\'s a draw! Try again";
+     } else if (computerSelection === "SCISSORS" && playerSelection == "PAPER"){
+       para1.textContent = "You loose! Scissors beat Paper";
+       cScore.textContent = "Computer score is " + computerScore++;
+     } else if (computerSelection === "SCISSORS" && playerSelection == "ROCK"){
+       para1.textContent = "You win! Rock beats Scissors";
+       pScore.textContent = "Your score is " + playerScore++;
+     } else if (computerSelection === "SCISSORS" && playerSelection == "SCISSORS"){
+       para1.textContent = "It\'s a draw! Try again";
        
-   } else { alert("Error! Play Rock or Paper or Scissors")
-   }
+   //} else { para1.textContent = "Error! Play Rock or Paper or Scissors";
+    }
 
    
-}
+   const endGame = document.querySelector('.endgame');
 
+   if (playerScore == 6){
 
-// comparing the player input and computer random result and giving a output
-
-//function game(){
-
+    //endGame.textContent = 'Game End...You won the Game...Refresh page to play again';
+     alert("Game End...You won the Game...Click Ok to play again")
     
-    
-    for (let i=0; i<=5; i++){
-        if (i==5){
-            alert("Refresh the page to Play Again");
-            break;
-        } else {
-  const playerSelection = prompt("make your choice:").toUpperCase();
+     location.reload();
+
+   } else if (computerScore == 6 ){
+       //endGame.textContent ="Game End...You Lossed the Game...Refresh page to play again";
+      alert("Game End...You Lossed the Game...Click Ok to play again")
+      location.reload();
+   }
   
-  //const playerSelection = "Rock";
-  const computerSelection = computerPlay();
-  console.log(playRound(playerSelection, computerSelection));
+   
+ }; 
 
-        }
 
-    }
+  //const playerSelection = prompt("make your choice:").toUpperCase();   
+       
+    playRound(playerSelection, computerSelection);
     
+      
+ })
+})
 
-    playRound(playerSelection, computerSelection) += [i];  
-        
-//}
 
-//game();
+  
 
